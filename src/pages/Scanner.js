b0f18821f33e0232b89ae340368a5941dcc84d2d
usmansabir98 <usmansabir98@hotmail.com>
2020-10-07 07:28:36 +0500

    final FYP

--- src/pages/Scanner.js
+++ src/pages/Scanner.js
@@ -4,6 +4,7 @@ import Container from '@material-ui/core/Container';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import '../css/styles.css';
+import axios from 'axios';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Timeline from '@material-ui/lab/Timeline';
@@ -36,17 +37,18 @@ function Scanner(props){
 
     const [delay, setDelay] = useState();
     const [result, setResult] = useState('No result');
+    const [supplyChain, setSupplyChain] = useState([]);
     const [show, setShow] = useState(false);
 
-    useEffect(()=> {
-        let information = "GTIN:12345,BatchNo:B11,SerialNumber:12";
-        let infoArr = information.split(',');
-        let result = information.split(',').map(s => s.split(':')).slice(0);
-        const obj = Object.fromEntries(result);
-        console.log("Info: ", infoArr);
-        console.log("Res: ", result);
-        console.log(obj);
-    });
+    // useEffect(()=> {
+    //     let information = "GTIN:12345,BatchNo:B11,SerialNumber:12";
+    //     let infoArr = information.split(',');
+    //     let result = information.split(',').map(s => s.split(':')).slice(0);
+    //     const obj = Object.fromEntries(result);
+    //     console.log("Info: ", infoArr);
+    //     console.log("Res: ", result);
+    //     console.log(obj);
+    // });
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -54,10 +56,28 @@ function Scanner(props){
     const handleScan = data => {
         if (data){
             setResult(data);
+
+            let information = data;
+            let infoArr = information.split(',');
+            let result = information.split(',').map(s => s.split(':')).slice(0);
+            const obj = Object.fromEntries(result);
+            console.log("Info: ", infoArr);
+            console.log("Res: ", result);
+            console.log(obj);
+
+            if(obj.GTIN!=null){
+                axios.post("https://localhost:44319/api/qrcode/scanQR", obj)
+            .then((res)=>{
+                const movements = res.data
+                setSupplyChain(movements);
+
+            })
+            }
             // this.setState({
             //     result: data
             // })
             handleShow();
+
         }
     }
 
@@ -80,30 +100,36 @@ function Scanner(props){
                     Launch demo modal
                 </Button> */}
 
-                <Modal show={show} onHide={handleClose} centered>
+                <Modal show={show} onHide={handleClose} centered size ="lg" >
                     <Modal.Header closeButton>
                     <Modal.Title>QR Code Result</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         {result}
                         <Timeline align="alternate">
-                            <TimelineItem>
-                                <TimelineSeparator>
-                                <TimelineDot>
-                                    <FontAwesomeIcon size = "2x" icon={faIndustry} />
-                                </TimelineDot>
-                                <TimelineConnector />
-                                </TimelineSeparator>
-                                <TimelineContent>
-                                <Paper elevation={3} className={classes.paper}>
-                                    <Typography variant="h6" component="h1">
-                                    Eat
-                                    </Typography>
-                                    <Typography>Because you need strength</Typography>
-                                </Paper>
-                                </TimelineContent>
-                            </TimelineItem>
-                            <TimelineItem>
+                            {
+                                supplyChain.map((item)=>{
+                                    return (<TimelineItem>
+                                    <TimelineSeparator>
+                                    <TimelineDot>
+                                        <FontAwesomeIcon size = "2x" icon={faIndustry} />
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <TimelineContent>
+                                    <Paper elevation={3} className={classes.paper}>
+                                        <Typography variant="h6" component="h1">
+                                        {item.text}
+                                        </Typography>
+                                        <Typography>{item.time}</Typography>
+                                        <Typography>{item.latitude}</Typography>
+                                        <Typography>{item.longitude}</Typography>
+                                    </Paper>
+                                    </TimelineContent>
+                                </TimelineItem>)
+                                })
+                            }
+                            {/* <TimelineItem>
                                 <TimelineSeparator>
                                 <TimelineDot>
                                     <FontAwesomeIcon size = "2x" icon={faIndustry} />
@@ -150,7 +176,7 @@ function Scanner(props){
                                     <Typography>Because it&apos;s awesome!</Typography>
                                 </Paper>
                                 </TimelineContent>
-                            </TimelineItem>
+                            </TimelineItem> */}
                         </Timeline>
                     </Modal.Body>
                     <Modal.Footer>
